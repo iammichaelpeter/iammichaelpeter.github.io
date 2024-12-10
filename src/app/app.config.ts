2e1e3622499360411861c5346280fcd2fff3e63e
Michael Peter <michael.pme@t-online.de>
2024-12-10 21:51:11 +0100

    chore: Configure base application setup, remove SSR and add styling structure

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,36 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { APP_INITIALIZER, ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+import { provideTransloco, TranslocoService } from '@jsverse/transloco';
+import { preloadTranslations, TranslocoHttpLoader } from './core/services/transloco-loader';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [
+    provideRouter(
+      routes,
+      withViewTransitions(),
+      withComponentInputBinding(),
+    ),
+    provideAnimationsAsync(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
+    provideTransloco({
+      config: {
+        availableLangs: ['de', 'en'],
+        defaultLang: 'de',
+        fallbackLang: 'de',
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode(),
+      },
+      loader: TranslocoHttpLoader
+    }),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: preloadTranslations,
+      deps: [TranslocoService],
+      multi: true
+    }
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,64 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Portfolio | Michael Peter',
+    loadComponent: () => import('./pages/landing/landing.component')
+      .then(m => m.LandingComponent),
+    data: {
+      meta: {
+        description: 'Hi, ich bin Michael! Entdecke mein Portfolio als Web Developer.'
+      }
+    }
+  },
+  {
+    path: 'projects',
+    title: 'Projects | Michael Peter',
+    loadComponent: () => import('./pages/project-list/project-list.component')
+      .then(m => m.ProjectListComponent),
+    data: {
+      meta: {
+        description: 'Entdecke meine aktuellen Web Development Projekte und deren Umsetzung.'
+      }
+    }
+  },
+  {
+    path: 'legal',
+    children: [
+      {
+        path: 'imprint',
+        loadComponent: () => import('./pages/imprint/imprint.component')
+          .then(m => m.ImprintComponent),
+        title: 'Imprint | Michael Peter',
+        data: {
+          meta: {
+            description: 'Impressum und Kontaktinformationen.'
+          }
+        }
+      },
+      {
+        path: 'privacy-policy',
+        loadComponent: () => import('./pages/legal/legal.component')
+          .then(m => m.LegalComponent),
+        title: 'Privacy Policy | Michael Peter',
+        data: {
+          meta: {
+            description: 'Rechtliche Hinweise und Datenschutzerklärung.'
+          }
+        }
+      }
+    ]
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+    title: '404 - Page Not Found | Michael Peter',
+    data: {
+      meta: {
+        description: 'Die gesuchte Seite wurde leider nicht gefunden.'
+      }
+    }
+  }
+];
